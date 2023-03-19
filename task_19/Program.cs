49d8c1c2f59ccecba8ef4de4e05335b2f3234639
Anastasia Novitskaya <nastasya11011995@gmail.com>
2023-03-20 10:11:30 +1200

    task_19

---
+++ task_19/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int temp = num;
+int revNum = 0;
+int temp2;
+while (num > 0)
+{
+    temp2 = num % 10;
+    revNum = revNum * 10 + temp2;
+    num = num / 10;
+}
+if (revNum == temp)
+{
+    Console.WriteLine("Число является палиндромом");
+}
+else
+{
+    Console.WriteLine("Число не является палиндромом");
+}
