7e443c2d5e68bc77d77c1116d4bdb4fa78ecad8d
Anastasia Novitskaya <nastasya11011995@gmail.com>
2023-03-20 12:30:30 +1200

    task_23

---
+++ task_23/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите N: ");
+int num = Convert.ToInt32(Console.ReadLine());
+void Cube(int num)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        Console.WriteLine(i * i * i);
+    }
+}
+Cube(num);
